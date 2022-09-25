a3f69683da04ad750ad192b350f286cec5f65a82
rafahk2o <49276128+rafahk2o@users.noreply.github.com>
2022-09-25 13:54:52 -0300

    remove maincontent

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import MainContent from "./components/MainContent";
+
 
 import "./styles/main.sass";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-    <MainContent />
   </React.StrictMode>
 );
--- vite.config.js
+++ vite.config.js
@@ -6,3 +6,5 @@ export default defineConfig({
   base:'./' ,
   plugins: [react()]
 })
+
+
