e6b93b90cb27b004a363f0f87a869c5d81ec83f7
rafahk2o <49276128+rafahk2o@users.noreply.github.com>
2022-09-25 13:38:07 -0300

    main content

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import MainContent from "./components/MainContent";
 
-import './styles/main.sass'
+import "./styles/main.sass";
 
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
+    <MainContent />
   </React.StrictMode>
-)
+);
