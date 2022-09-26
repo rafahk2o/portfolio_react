b8c313de8e14218c23934b8aaa92984686ab0b92
rafahk2o <49276128+rafahk2o@users.noreply.github.com>
2022-09-26 14:03:26 -0300

    projetos link

--- src/components/ProjetosContainer.jsx
+++ src/components/ProjetosContainer.jsx
@@ -4,7 +4,7 @@ const ProjetosContainer = () => {
   return <section className="projects-container">
     <h2>Projetos</h2>
     <p>Projetos elaborados com o objetivo de treino e ao mesmo passo atendendo à demandas reiais de uma empresa no dia-a-dia.</p>
-        <a href="https://rafahk2o.github.io/Meu-portfolio/" className="btn">
+        <a href="https://rafahk2o.github.io/Meu-portfolio/#projetos" className="btn">
             Ver projetos
         </a>
   </section>
